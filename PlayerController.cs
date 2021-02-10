a161a54011983e84a9c546e7fd84ab4c1318fbd1
luanalucato <71470261+luanalucato@users.noreply.github.com>
2021-02-10 12:12:25 +0000

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+   public Rigidbody2D rb;
+
+   public float moveSpeed = 5;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDirection = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(0, moveDirection * moveSpeed);
+
+    }
+
+    Void OnTriggerEnter2D(Collider2D other)
+{
+    if(other.gameObject.CompareTag("Obstacle"))
+    {
+        Destroy(gameObject);
+    }
+}
+
+
+}
+
---
+++ backgraundScroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgraundScroller : MonoBehaviour
+{
+    public BoxCollider2D collider;
+    
+    public Rigidbody2D rb;
+     
+    private float width;
+
+    private float scrollSpeed = -2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent <BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = collider.size.x;
+        collider.enabled = false;
+
+        rb.velocity = new Vector2(scrollSpeed, 0);
+        resetObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+    {
+        if (transform.position.x < -width)
+       {
+         Vector2 restPosition = new Vector2(width * 2f, 0);
+         transform.position = (Vector2)transform.position + restPosition;
+         resetObstacle();
+         
+        
+       }
+
+    }
+}
+
+void resetObstacle()
+{
+    transform.getChild(0).localPosition = new Vector3(0, rambom.range(-3,3), 0);
+}
